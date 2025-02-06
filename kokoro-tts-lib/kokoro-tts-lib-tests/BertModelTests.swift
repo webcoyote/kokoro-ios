25b26ab4f68444a8d979a4a0d27ab44d76c8cd82
mlalma <lassi.maksimainen@gmail.com>
2025-02-06 13:07:28 +0100

    Added Bert models for the forward pass

---
+++ kokoro-tts-lib/kokoro-tts-lib-tests/BertModelTests.swift
@@ -0,0 +1,16 @@
+//
+//  kokoro-tts-lib
+//
+import Testing
+@testable import KokoroTTSLib
+
+struct BertModelTests {
+    @Test func testBertModel() async throws {
+        let tokens = [50, 157, 43, 135, 16, 53, 135, 46, 16, 43, 102, 16, 56, 156, 57, 135, 6, 16, 102, 62, 61, 16, 70, 56, 16, 138, 56, 156, 72, 56, 61, 85, 123, 83, 44, 83, 54, 16, 53, 65, 156, 86, 61, 62, 131, 83, 56, 4, 16, 54, 156, 43, 102, 53, 16, 156, 72, 61, 53, 102, 112, 16, 70, 56, 16, 138, 56, 44, 156, 76, 158, 123, 56, 16, 62, 131, 156, 43, 102, 54, 46, 16, 102, 48, 16, 81, 47, 102, 54, 16, 54, 156, 51, 158, 46, 16, 70, 16, 92, 156, 135, 46, 16, 54, 156, 43, 102, 48, 4, 16, 81, 47, 102, 16, 50, 156, 72, 64, 83, 56, 62, 16, 156, 51, 158, 64, 83, 56, 16, 44, 157, 102, 56, 16, 44, 156, 76, 158, 123, 56, 4]
+        
+        let model = try BertModel()
+        let output = try model.forwardPassOnInitialModel(tokens: tokens)
+        let _ = try model.forwardPassOnEncoderModel(array: output)
+    }
+}
+
