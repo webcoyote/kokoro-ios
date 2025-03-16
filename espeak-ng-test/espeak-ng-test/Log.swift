b63bf712f3c5d4c41f50286619f5b53f3b7ddef5
mlalma <lassi.maksimainen@gmail.com>
2025-03-16 18:11:39 +0100

    Removed eSpeak NG test application, not needed anymore

--- espeak-ng-test/espeak-ng-test/Log.swift
+++
@@ -1,8 +0,0 @@
-import os
-
-let logger = Logger(subsystem: "com.kokoro.espeak-ng-test", category: "app")
-
-func log(_ message: String) {
-    logger.log("\(message)")
-}
-
--- espeak-ng-test/espeak-ng-test/espeak-ng-test-Bridging-Header.h
+++
@@ -1,1 +0,0 @@
-#include "ESpeakNGTest.h"
