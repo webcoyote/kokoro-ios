bc3d505603f92f22d6cab4343c824d21f613da4f
mlalma <lassi.maksimainen@gmail.com>
2025-09-08 20:08:57 +0200

    Added the limits for the amount of memory that MLX will keep after it is used

--- mlxtest/mlxtest/mlxtestApp.swift
+++ mlxtest/mlxtest/mlxtestApp.swift
@@ -1,8 +1,14 @@
 import SwiftUI
+import MLX
 
 @main
 struct mlxtestApp: App {
   let model = MLXTestModel()
+    
+  init() {
+    MLX.GPU.set(cacheLimit: 50 * 1024 * 1024)
+    MLX.GPU.set(memoryLimit: 900 * 1024 * 1024)
+  }
 
   var body: some Scene {
     WindowGroup {
