bed556a52a372a03bf6026ebda192c9ea8f73017
Patrick Wyatt <patrickjohnwyatt@gmail.com>
2025-10-25 18:34:26 -0700

    Update Package.swift to use process instead of copy

--- Package.swift
+++ Package.swift
@@ -34,7 +34,7 @@ let package = Package(
         .product(name: "MLXUtilsLibrary", package: "MLXUtilsLibrary")
       ],
       resources: [
-       .copy("Resources/")
+          .process("Resources/", localization: .none)
       ]
     ),
     .testTarget(
